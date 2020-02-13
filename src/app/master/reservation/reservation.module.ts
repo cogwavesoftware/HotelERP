8a713c3eac1cbe5a520ede90377c0d6b733354f9
cogwavesoftware <cogwavesoftware@gmail.com>
2020-02-13 15:51:20 +0530

    aff

--- src/app/master/reservation/reservation.module.ts
+++ src/app/master/reservation/reservation.module.ts
@@ -15,8 +15,7 @@ import { ReservationRoutingModule } from './reservation-routing.module';
 
 import {DatePipe} from '@angular/common';
 import { NgSelectModule } from '@ng-select/ng-select';
-import { AutofocusDirective } from './autofocus.directive';
-@NgModule({
+ @NgModule({
   declarations: [ReservationComponent],
   //declarations: [ReservationComponent,ConfirmationDialogComponent],
   providers: [DatePipe,ConfirmationDialogService],
