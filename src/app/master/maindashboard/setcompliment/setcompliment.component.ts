04b918c3543c5428bcc0e362c06a166fa63e3c5b
cogwavesoftware <cogwavesoftware@gmail.com>
2020-05-11 16:17:53 +0530

    aa

--- src/app/master/maindashboard/setcompliment/setcompliment.component.ts
+++ src/app/master/maindashboard/setcompliment/setcompliment.component.ts
@@ -1,71 +1,64 @@
-import { Component, OnDestroy, OnInit, ViewEncapsulation, ViewChild, ElementRef,Inject, PLATFORM_ID, } from "@angular/core";
- 
+import { Component, OnInit, ViewChild } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { Observable } from "rxjs"; 
+import { IpserviceService } from "src/app/_services/ipservice.service";
 
+import { ToastData,ToastOptions,ToastyService } from 'ng2-toasty'
+import { MasterformService } from 'src/app/_services/masterform.service';
 @Component({
   selector: 'app-setcompliment',
   templateUrl: './setcompliment.component.html',
   styleUrls: ['./setcompliment.component.scss']
 })
 export class SetcomplimentComponent implements OnInit {
-  rows1 = []; 
-  selected = []; 
-  constructor() {
-    this.fetch((data) => {
-      this.rows1 = data;
-    });
+  public data: Observable<any>;
+  public rowsOnPage = 10;
+  public filterQuery = "";
+  public sortBy = "";
+  public sortOrder = "desc";
+  model: any = {};   
+  Branch:string;
+  filterdata:any;
+  emailFormArray: Array<any> = [];
+  @ViewChild("f", { static: false }) form: any;  
+  categories: any[];
+  constructor(private _masterformservice: MasterformService,
+    private _ipservice: IpserviceService,
+    private toastyService: ToastyService) {
+      this.Branch= localStorage.getItem("BranchCode");
    }
 
   ngOnInit() {    
-  }
-  fetch(cb) {
-    const req = new XMLHttpRequest();
-    req.open('GET', `assets/data/company.json`);
-    req.onload = () => {
-      cb(JSON.parse(req.response));
-    };
 
-    req.send();
-  }
-  onSelect({ selected }) { 
- 
-    this.selected.splice(0, this.selected.length);
+    this.data = this._masterformservice.getrevenudata(this.Branch);
 
-    this.selected.push(...selected);
-
-    console.log(selected); 
-  }
- 
-  //complementcheck(event){
-   // alert("complet");
-   // console.log(event.target.value);
-
-    //if(this.complementisSelected == true){
-    //  this.complementisSelected = true;
-      //this.blockisSelected=false; 
-    //}
-    
- // }
-  //blockcheck(event){
-    //alert("block");
-  //  console.log(event.target.value);
-    //if(this.blockisSelected == true){
-     // this.blockisSelected = true;
-     // this.complementisSelected=false; 
-   // }
-
-  //}
-  //onCheckboxChangeFn(event){
-   
-  //}
-  onActivate(event) {
+    this._masterformservice.getrevenudata(this.Branch).subscribe(data=>{
+      this.filterdata=data;
+    });
      
-  }
-
+     this._masterformservice.getothertax(this.Branch).subscribe((data: any) => {
+       data.forEach(obj => (obj.selected = false));
+       this.categories = data;
+       console.log(this.categories);
+     }); 
  
-
-  remove() {
-    this.selected = [];
   }
+  
+  onSubmit(form?: NgForm) {  
+    alert("jai");
+    console.log("form value " + form.value);  
+    console.log("array value "+ this.emailFormArray);
+  }
+
+  onChange(ids: number, isChecked: boolean) {
+    if (isChecked) {
+      this.emailFormArray.push(ids);
+    } else {
+      let index = this.emailFormArray.indexOf(ids);
+      this.emailFormArray.splice(index, 1);
+    }
+  } 
+ 
   closeMyModalPin(event) { 
     var openModals = document.querySelectorAll(".md-show");
     for (let i = 0; i < openModals.length; i++) {
